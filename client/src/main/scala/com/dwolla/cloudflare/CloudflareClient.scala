6773895a1210e27232f9329c98f71a1f7b445daf
Nick Leeper <nick.leeper@gmail.com>
2018-06-29 14:45:36 -0500

    moving away from supplying executor to clients, and creating unified client

--- client/src/main/scala/com/dwolla/cloudflare/CloudflareClient.scala
+++ client/src/main/scala/com/dwolla/cloudflare/CloudflareClient.scala
@@ -1,10 +1,36 @@
 package com.dwolla.cloudflare
 
+import java.io.Closeable
+
 import cats.Monad
+import cats.effect.Async
 import com.dwolla.cloudflare.clients.{AccountsClient, DnsRecordClient, RateLimitClient}
 
-class CloudflareClient[F[_] : Monad](executor: CloudflareApiExecutor[F]) {
-  lazy val Accounts = new AccountsClient(executor)
-  lazy val RateLimits = new RateLimitClient(executor)
-  lazy val Dns = new DnsRecordClient(executor)
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.higherKinds
+
+trait CloudflareClient[F[_]] {
+  def Accounts: AccountsClient[F]
+  def RateLimits: RateLimitClient[F]
+  def Dns: DnsRecordClient[F]
+}
+
+class FutureCloudflareClient(authorization: CloudflareAuthorization)(implicit ec: ExecutionContext, implicit val evidence: Monad[Future]) extends CloudflareClient[Future] with Closeable {
+  private val executor = new FutureCloudflareApiExecutor(authorization)
+
+  override lazy val Accounts = new AccountsClient(executor)
+  override lazy val RateLimits = new RateLimitClient(executor)
+  override lazy val Dns = new DnsRecordClient(executor)
+
+  override def close(): Unit = executor.close()
+}
+
+class AsyncCloudflareClient[F[_]: Async](authorization: CloudflareAuthorization)(implicit ec: ExecutionContext) extends CloudflareClient[F] with Closeable {
+  private val executor = new AsyncCloudflareApiExecutor(authorization)
+
+  override lazy val Accounts = new AccountsClient(executor)
+  override lazy val RateLimits = new RateLimitClient(executor)
+  override lazy val Dns = new DnsRecordClient(executor)
+
+  override def close(): Unit = executor.close()
 }
