db8446eec1f9ef825890de547f236e38c9da21f5
Brian P. Holt <bholt@dwolla.com>
2018-07-24 11:37:09 -0500

    remove json4s and Apache HTTP client from project

--- dto/src/main/scala/com/dwolla/cloudflare/domain/dto/ratelimits/RateLimitActionDTO.scala
+++ dto/src/main/scala/com/dwolla/cloudflare/domain/dto/ratelimits/RateLimitActionDTO.scala
@@ -1,14 +1,12 @@
 package com.dwolla.cloudflare.domain.dto.ratelimits
 
-import com.dwolla.cloudflare.domain.dto.JsonWritable
-
 case class RateLimitActionDTO (
   mode: String,
   timeout: Int,
   response: Option[RateLimitActionResponseDTO]
-) extends JsonWritable
+)
 
 case class RateLimitActionResponseDTO (
   content_type: String,
   body: String
-) extends JsonWritable
+)
