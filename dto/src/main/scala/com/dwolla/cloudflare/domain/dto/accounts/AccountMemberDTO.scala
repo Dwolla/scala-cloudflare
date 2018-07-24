f909f762ebb5c7610af9a4506927b80b78a13eff
Brian P. Holt <bholt@dwolla.com>
2018-07-24 11:37:09 -0500

    remove json4s and Apache HTTP client from project

--- dto/src/main/scala/com/dwolla/cloudflare/domain/dto/accounts/AccountMemberDTO.scala
+++ dto/src/main/scala/com/dwolla/cloudflare/domain/dto/accounts/AccountMemberDTO.scala
@@ -1,10 +1,8 @@
 package com.dwolla.cloudflare.domain.dto.accounts
 
-import com.dwolla.cloudflare.domain.dto.JsonWritable
-
 case class AccountMemberDTO (
   id: String,
   user: UserDTO,
   status: String,
   roles: Seq[AccountRoleDTO]
-) extends JsonWritable
+)
