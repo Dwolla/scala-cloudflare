9f2971182fa97127cf2dd625eaab57c89ac07b28
Nick Leeper <nick.leeper@gmail.com>
2018-09-10 16:15:06 -0500

    add uri method to RateLimit

--- client/src/main/scala/com/dwolla/cloudflare/domain/model/accounts/AccountMember.scala
+++ client/src/main/scala/com/dwolla/cloudflare/domain/model/accounts/AccountMember.scala
@@ -1,12 +1,14 @@
 package com.dwolla.cloudflare.domain.model.accounts
 
+import com.dwolla.cloudflare.BaseUrl
+import org.http4s.Uri
+
 case class AccountMember (
   id: String,
   user: User,
   status: String,
   roles: Seq[AccountRole]
 ) {
-  def uri(accountId: String): String = {
-    s"https://api.cloudflare.com/client/v4/accounts/$accountId/members/$id"
-  }
+  def uri(accountId: String): Uri =
+    BaseUrl / "accounts" / accountId / "members" / id
 }
--- client/src/main/scala/com/dwolla/cloudflare/domain/model/ratelimits/RateLimit.scala
+++ client/src/main/scala/com/dwolla/cloudflare/domain/model/ratelimits/RateLimit.scala
@@ -1,5 +1,8 @@
 package com.dwolla.cloudflare.domain.model.ratelimits
 
+import com.dwolla.cloudflare.BaseUrl
+import org.http4s.Uri
+
 case class RateLimit (
   id: String,
   disabled: Option[Boolean] = None,
@@ -9,7 +12,10 @@ case class RateLimit (
   threshold: Int,
   period: Int,
   action: RateLimitAction
-)
+) {
+  def uri(zoneId: String): Uri =
+    BaseUrl / "zones" / zoneId / "rate_limits" / id
+}
 
 case class CreateRateLimit (
   disabled: Option[Boolean] = None,
