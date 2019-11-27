3b2d79c2c716da41e980f7309bd0a0589e18dab4
Corey Oliver <corey@dwolla.com>
2019-11-27 13:47:40 -0600

    add ability to enable waf

--- client/src/main/scala/com/dwolla/cloudflare/domain/model/ZoneSettings.scala
+++ client/src/main/scala/com/dwolla/cloudflare/domain/model/ZoneSettings.scala
@@ -6,38 +6,54 @@ import io.circe.generic.semiauto._
 
 object ZoneSettings {
   sealed trait CloudflareTlsLevel
+  object CloudflareTlsLevel {
+    case object Off extends CloudflareTlsLevel
+    case object FlexibleTls extends CloudflareTlsLevel
+    case object FullTls extends CloudflareTlsLevel
+    case object FullTlsStrict extends CloudflareTlsLevel
+    case object StrictTlsOnlyOriginPull extends CloudflareTlsLevel
 
-  case object Off extends CloudflareTlsLevel
-  case object FlexibleTls extends CloudflareTlsLevel
-  case object FullTls extends CloudflareTlsLevel
-  case object FullTlsStrict extends CloudflareTlsLevel
-  case object StrictTlsOnlyOriginPull extends CloudflareTlsLevel
-
-  implicit val cloudflareTlsLevelEncoder: Encoder[CloudflareTlsLevel] =
-    encoderBuilder[CloudflareTlsLevel] {
-      case Off => "off"
-      case FlexibleTls => "flexible"
-      case FullTls => "full"
-      case FullTlsStrict => "strict"
-      case StrictTlsOnlyOriginPull => "origin_pull"
-    }
+    implicit val cloudflareTlsLevelEncoder: Encoder[CloudflareTlsLevel] =
+      encoderBuilder[CloudflareTlsLevel] {
+        case Off => "off"
+        case FlexibleTls => "flexible"
+        case FullTls => "full"
+        case FullTlsStrict => "strict"
+        case StrictTlsOnlyOriginPull => "origin_pull"
+      }
+  }
 
   sealed trait CloudflareSecurityLevel
+  object CloudflareSecurityLevel {
+    case object EssentiallyOff extends CloudflareSecurityLevel
+    case object Low extends CloudflareSecurityLevel
+    case object Medium extends CloudflareSecurityLevel
+    case object High extends CloudflareSecurityLevel
+    case object UnderAttack extends CloudflareSecurityLevel
 
-  case object EssentiallyOff extends CloudflareSecurityLevel
-  case object Low extends CloudflareSecurityLevel
-  case object Medium extends CloudflareSecurityLevel
-  case object High extends CloudflareSecurityLevel
-  case object UnderAttack extends CloudflareSecurityLevel
+    implicit val cloudflareSecurityLevelEncoder: Encoder[CloudflareSecurityLevel] =
+      encoderBuilder[CloudflareSecurityLevel] {
+        case EssentiallyOff => "essentially_off"
+        case Low => "low"
+        case Medium => "medium"
+        case High => "high"
+        case UnderAttack => "under_attack"
+      }
+  }
 
-  implicit val cloudflareSecurityLevelEncoder: Encoder[CloudflareSecurityLevel] =
-    encoderBuilder[CloudflareSecurityLevel] {
-      case EssentiallyOff => "essentially_off"
-      case Low => "low"
-      case Medium => "medium"
-      case High => "high"
-      case UnderAttack => "under_attack"
-    }
+  sealed trait CloudflareWaf
+  object CloudflareWaf {
+    case object Off extends CloudflareWaf
+    case object On extends CloudflareWaf
+
+    implicit val cloudflareWafEncoder: Encoder[CloudflareWaf] =
+      encoderBuilder[CloudflareWaf] {
+        case Off => "off"
+        case On => "on"
+      }
+  }
+
+
 
   private def encoderBuilder[T](f: T => String): Encoder[T] = Encoder[CloudflareSettingValue].contramap(f.andThen(CloudflareSettingValue(_)))
 
@@ -52,4 +68,5 @@ object ZoneSettings {
 case class Zone(name: String,
                 tlsLevel: CloudflareTlsLevel,
                 securityLevel: Option[CloudflareSecurityLevel],
+                waf: Option[CloudflareWaf],
                )
